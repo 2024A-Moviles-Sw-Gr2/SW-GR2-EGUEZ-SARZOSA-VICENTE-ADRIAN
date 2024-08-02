f4aab86d681007be79847aaa5bab903524f2aaee
Adrian Eguez <eadepto@hotmail.com>
2024-08-02 10:39:59 -0500

    fin

--- 01-clases/app/build.gradle.kts
+++ 01-clases/app/build.gradle.kts
@@ -43,6 +43,9 @@ android {
 
 dependencies {
 
+    implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
